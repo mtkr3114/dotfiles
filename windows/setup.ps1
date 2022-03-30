b52da9c8378fa60b90587a5932565dd9933805b1
watarukt <55861560+watarukt@users.noreply.github.com>
2022-03-30 10:21:23 +0900

    fix setup.ps1 (add install module and organize code)

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,6 +1,9 @@
-# install scoop
+# windows setup script
+# 1. install scoop
+# 2. set utf8
+# todo nerdfont install
 
-# install dir
+# 1. install scoop
 try {
     get-command scoop -ErrorAction Stop
 }
@@ -11,34 +14,39 @@ catch [Exception] {
 
 $SCOOP_ROOT = if ($env:SCOOP) {$env:SCOOP} else {"$home\scoop"}
 
-# install basic module
+# basic module
 scoop install aria2
+
 scoop install git
 scoop install curl
 scoop install sudo
 
 # add bucket 
 scoop bucket add extras
-scoop bucket add versions
-
-# todo nerdfont install
-
-# chrome
-scoop install googlechrome
-
-# vscode
 scoop install vscode
-# error発生
-# ファイルがあるかを確認
-#reg import $SCOOP_ROOT\apps\vscode\current\vscode-install-context.reg
 
-# slack
+# editor
+scoop install vim
+scoop install neovim
+
+# terminal
+scoop install teraterm
+scoop install windows-terminal
+scoop install alacritty
+
+# win-app
+scoop install wireshark
 scoop install slack
 
-#scoop install vcredist2019
+# browser
+scoop install googlechrome
 
-# べつのファイルに切り出す
-# utf8
+# personal
+scoop install vivaldi
+scoop install autohotkey
+scoop install neovide
+
+# 2. set utf8
 # PowerShell core7でもConsoleのデフォトエンコーディングはsjisなので必要
 [System.Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
 [System.Console]::InputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
